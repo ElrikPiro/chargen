d1f48c9381f472fd8b4d2461f2efe0818ddc87bf
David Baselga <baselga.david@gmail.com>
2022-07-10 18:01:00 +0200

    shortest path dijkstra

--- chargen/__init__.py
+++ chargen/__init__.py
@@ -4,3 +4,4 @@ from .core import RelationType
 from .character import Caller
 from .character import Character
 from .render import markdownGenerator
+from .grafolocalizaciones import GrafoLocalizaciones, LocalizacionesNodo
