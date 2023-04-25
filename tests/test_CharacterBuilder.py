00f08284af8f79c413399864221dd5d206d613b8
Baselga Masia, David (Vaio-Linux) <baselga.david@gmail.com>
2023-04-25 19:01:00 +0200

    stopped at test failure

--- tests/test_CharacterBuilder.py
+++ tests/test_CharacterBuilder.py
@@ -10,6 +10,22 @@ class CharacterBuilderTest(unittest.TestCase):
         builder = chargen.CharacterBuilder()
         self.assertIsNotNone(builder.settings_["characterId"])
 
+    def test_init_default_persistenceIsTinyDB(self):
+        builder = chargen.CharacterBuilder()
+        self.assertEqual(builder.settings_["persistence"], "tinyDB")
+
+    def test_init_default_modulesIsEmpty(self):
+        builder = chargen.CharacterBuilder()
+        self.assertEqual(builder.settings_["modules"], {})
+
+    def test_init_characterIdIsSet_characterIdIsSet(self):
+        builder = chargen.CharacterBuilder(characterId = "test")
+        self.assertEqual(builder.settings_["characterId"], "test")
+
+    def test_init_persistenceIsInvalid_exception(self):
+        with self.assertRaises(ValueError):
+            chargen.CharacterBuilder(persistence = "invalid")
+
 
 if __name__ == '__main__':
     unittest.main()
